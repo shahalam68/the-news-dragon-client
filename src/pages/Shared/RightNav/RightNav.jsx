9a7ec10540b44aed77ee5f901e4cd01edf3be34d
Soykot Alam <a.k.mshahalam68@gmail.com>
2023-09-14 20:34:50 +0600

    first commit

--- src/pages/Shared/RightNav/RightNav.jsx
+++ src/pages/Shared/RightNav/RightNav.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Button, ListGroup } from 'react-bootstrap';
+import { FaGithub, FaGofore,FaFacebook,FaTwitter,FaInstagramSquare } from 'react-icons/fa';
+import QZone from '../QZone/QZone';
+import bg from '../../../assets/bg.png'
 
 const RightNav = () => {
     return (
         <div>
-            <h2>this is right nav</h2>
+            <h4>this is right nav</h4>
+            <Button className='mb-2 ' variant="outline-primary"><FaGofore /> Login With Google</Button>
+            <Button className='mb-4' variant="outline-secondary"><FaGithub /> Login With gitHub</Button>
+            <div >
+                <h4>Find Us On</h4>
+                <ListGroup>
+                    <ListGroup.Item><FaFacebook/> Facebook</ListGroup.Item>
+                    <ListGroup.Item><FaTwitter/> Twitter</ListGroup.Item>
+                    <ListGroup.Item><FaInstagramSquare/> Instagram</ListGroup.Item>
+                </ListGroup>
+            </div>
+            <QZone></QZone>
+            <div>
+
+            </div>
         </div>
     );
 };
