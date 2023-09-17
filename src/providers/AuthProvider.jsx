1aa787444f87d3dc0fdb0154a05c996a94adcc51
Soykot Alam <a.k.mshahalam68@gmail.com>
2023-09-17 13:36:38 +0600

    provider

---
+++ src/providers/AuthProvider.jsx
@@ -0,0 +1,12 @@
+import React, { createContext } from 'react';
+
+const AuthProvider = () => {
+    const AuthContext = createContext(null);
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default AuthProvider;
