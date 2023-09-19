577d6d58a8ae7dc3b15dff4f8e5ba1ba02b377bb
Soykot Alam <a.k.mshahalam68@gmail.com>
2023-09-19 23:44:08 +0600

    login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
-import { Button, Container,Form } from 'react-bootstrap';
+import { Button, Container, Form } from 'react-bootstrap';
 
 
 const Login = () => {
     return (
         <Container className='mx-auto w-25'>
+            <h2>Please Login</h2>
             <Form>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">
-                    </Form.Text>
+                    <Form.Control type="email" name="email" placeholder="Enter email" required />
+
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control type="password" name="password" placeholder="Password" required />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
                     <Form.Check type="checkbox" label="Check me out" />
@@ -23,6 +23,10 @@ const Login = () => {
                 <Button variant="primary" type="submit">
                     Submit
                 </Button>
+                <Form.Text className="text-danger">
+                </Form.Text>
+                <Form.Text className="text-danger">
+                </Form.Text>
             </Form>
         </Container>
     );
