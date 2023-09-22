9dbf769abaf47289ab500679317e6b989b4fc755
Soykot Alam <a.k.mshahalam68@gmail.com>
2023-09-22 20:19:44 +0600

    location

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,12 +1,15 @@
 import React, { useContext } from 'react';
 import { Button, Container, Form } from 'react-bootstrap';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../providers/AuthProvider';
 
 
 const Login = () => {
     const { signIn } = useContext(AuthContext);
     const navigate = useNavigate();
+    const location = useLocation();
+    console.log('login page location', location);
+    const from = location.state?.from?.pathname || '/category/0'
 
     const handleSignIn = event => {
         event.preventDefault();
@@ -17,7 +20,7 @@ const Login = () => {
             .then(result => {
                 const loggedUser = result.user;
                 console.log(loggedUser);
-                navigate('/category/0')
+                navigate(from)
             })
             .catch(error => {
                 console.log(error);
